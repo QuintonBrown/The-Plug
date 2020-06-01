423f20abc0073241ee359db49615fd9d39887ef7
Quinton H Brown <quintonbrown8101@yahoo.com>
2020-06-01 16:02:35 -0400

    bugs

--- client/src/nav.jsx
+++ client/src/nav.jsx
@@ -8,7 +8,7 @@ class Nav extends React.Component {
                     <nav>
                         <ul>
                             <li><img className="logo" src="/ProductImages/The-Plug-Logo.jpg" alt="The Plug Logo" /></li>
-                                <li><a className="navbutton" href="index.html">Home</a></li>
+                                <li><a className="navbutton" href="home.html">Home</a></li>
                                 <li className="active"><a class="navbutton" href="products.html">Products</a></li>
                                 <li><a className="navbutton" href="contact.html">Contact</a></li>
                          </ul>
